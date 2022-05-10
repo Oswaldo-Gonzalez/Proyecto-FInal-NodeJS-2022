d555f9a423aa54827b233437acb30ba46e9e260b
Oswaldo <caballeorblack117@live.com.mx>
2022-05-10 16:27:27 -0500

    Added test with JSON database / act 5 video 01

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const {pokemon} = require('./pokedex.json')
 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send("Bienvenido");
+    res.send("Welcome to Taller de Node.js S.A. de C.V. employee system");
 });
 
-app.listen(3000, () => {
+app.get("/pokemon", (req, res, next) => {
+    res.status(200);
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id', (req, res, next) => {
+    res.status(200);
+    res.send(pokemon[req.params.id - 1]);
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
