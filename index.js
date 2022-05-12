04a76b549b918e55fb4c777a74e5be9de0415e49
Oswaldo <caballeorblack117@live.com.mx>
2022-05-11 19:21:47 -0500

    Added database and database connection from Act 7 / 06

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
-const {pokemon} = require('./pokedex.json')
+const employees = require('./routes/employees');
 
-app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Welcome to Taller de Node.js S.A. de C.V. employee system");
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => { // Root address
+   return res.status(200).send("Welcome to Taller de Node.js S.A. de C.V. employee system");
 });
 
-app.get("/pokemon", (req, res, next) => {
-    res.status(200);
-    res.send(pokemon);
-});
-
-app.get('/pokemon/:id', (req, res, next) => {
-    res.status(200);
-    res.send(pokemon[req.params.id - 1]);
-});
+app.use("/employees", employees);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
