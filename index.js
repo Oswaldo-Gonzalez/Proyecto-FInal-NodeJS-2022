313fec8ea5729c4d8e2f9bc63eb243ee53e4364c
Oswaldo <caballeorblack117@live.com.mx>
2022-05-13 14:21:37 -0500

    Added PUT function from Act 9

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => { // Root address
-   return res.status(200).send("Welcome to Taller de Node.js S.A. de C.V. employee system");
+   return res.status(200).json({code: 1, messsage: "Welcome to Taller de Node.js S.A. de C.V. employee system"});
 });
 
 app.use("/employees", employees);
+
+app.use((req, res, next) => {
+    return res.status(404).json({code: 404, message: "URL not found"});
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
--- routes/employees.js
+++ routes/employees.js
@@ -2,28 +2,42 @@ const express = require('express');
 const employees = express.Router();
 const db = require('../config/database');
 
-employees.post("/", (req, res, next) => {
-    return res.status(200).send(req.body.name);
+employees.post("/", async (req, res, next) => { // Adds a new employee to the database
+    const {Name, Last_Name, Phone_Number, Email, Address} = req.body;
+
+    if(Name && Last_Name && Phone_Number && Email && Address){
+        let query = "INSERT INTO employees (Name, Last_Name, Phone_Number, Email, Address)";
+        query += ` VALUES('${Name}', '${Last_Name}', '${Phone_Number}', '${Email}', '${Address}')`;
+
+        const rows = await db.query(query);
+
+        if(rows.affectedRows == 1){
+            return res.status(201).json({code: 201, message: "New employee added to system"});
+        }
+        return res.status(500).json({code: 500, message: "An error ocurred"})
+    }
+    return res.status(500).json({code: 500, message: "Incomplete fields"})
 });
 
 employees.get("/", async (req, res, next) => { // Returns all the employees
     const emp = await db.query("SELECT * FROM employees");
-    return res.status(200).json(emp);
+    return res.status(200).json({code: 200, message: emp});
 });
 
-employees.get('/:id([0-9]{1,3})', (req, res, next) => {
+/*employees.get('/:id([0-9]{1,3})', (req, res, next) => {
     const id = req.params.id - 1;
     return (id >= 0 && id <= 150) ? res.status(200).send(pk[req.params.id - 1]) : res.status(404).send("Pokémon no encontrado");
     
-});
+});*/
 
-employees.get('/:name([A-Za-z]+)', (req, res, next) => { // Returns an employee by name
-    const name = req.params.name;
-    const pkmn = pk.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
-    });
-
-    return (pkmn.length > 0) ? res.status(200).send(pkmn) : res.status(404).send("Pokémon no encontrado");
+employees.get('/:name([A-Za-z]+)', async (req, res, next) => { // Returns an employee by name
+    const emp_name = req.params.name;
+    const emp = await db.query("SELECT * FROM employees WHERE Name='"+emp_name+"';")
+    
+    if (emp.length > 0) {
+        return res.status(200).json({code: 200, message: emp});
+    }
+    return res.status(404).json({code: 404, message: "Employee not found"})
 });
 
 module.exports = employees;
